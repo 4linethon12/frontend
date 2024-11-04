f5cd507277ad97f517813ca5f9533c23eb1037ab
seongbeom <rhtmdqja120@gmail.com>
2024-11-04 18:21:05 +0900

    d

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>머지</h1>
+      <h1>what</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
