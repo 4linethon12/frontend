4de0e0976bc6cc0f6017aca06e3342f2faa77183
yuchae Park <20232293@sungshin.ac.kr>
2024-11-12 19:28:09 +0900

    feat: 심리테스트

--- src/pages/Test/TestMain.jsx
+++ src/pages/Test/TestMain.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import AllText from './js/text'
 
 const TestMain = () => {
   return (
@@ -6,14 +7,11 @@ const TestMain = () => {
         <div className='single_cancel'><img src={cancel} /></div>
         <div className='prograss_bar'></div>
         <div className='question'>
-            <p>친구와 잘 어울리는</p>
-            <p>크리스마스 명소는?</p>
+            <p>{AllText.question}</p>
         </div>
 
-        <div className='main_test'>
-            <span className='first'>🎄반짝이는 조명으로 가득한 숲 속 트리</span>
-            <span className='second'>❄️ 끝없이 펼쳐진 설원</span>
-            <span className='third'>🎅 캐롤이 울려 퍼지는 크리스마스 마을</span>
+        <div className='test_box'>
+            <button className='choice_btn'>{AllText.choice}</button>
         </div>
 
         <button>다음</button>
--- src/pages/Test/TestResult.jsx
+++ src/pages/Test/TestResult.jsx
@@ -1,19 +1,51 @@
 import React from 'react'
+import Character from './js/result'
+import togle from '../../assets/img/test/togle.svg'
 
-const TestResult = () => {
+const TestResult = ({ characterKey }) => {
+
+  const selectedCharacter = Character[characterKey];
+
   return (
     <div className='result_wrap'>
-        <h1 className='character'>부끄럼 많은 루돌프</h1>
-        <span className='character_pic' />
-        <div className='detail'>
-            <div className='container1'>
-                
-                <p>낯을 가리지만, 친해지면 누구보다 따뜻한 친구예요!</p>
-            </div>
-            
-        </div>
-    </div>
-  )
-}
+      <h1 className='character'>{selectedCharacter.title}</h1>
 
-export default TestResult
+      <span className='character_pic'>
+        {/* 캐릭터 이미지 로직 */}
+      </span>
+
+      <div className='detail'>
+        <div className='container1'>
+          <img src={togle} />
+          <p>{selectedCharacter.descnature}</p>
+        </div>
+        <div className='container1'>
+          <img src={togle}/>
+          <p>{selectedCharacter.desc}</p>
+        </div>
+      </div>
+
+      <span className='line' />
+
+      <div className='result_bottom'>
+        <div className='chemi_character1'>
+          <p className='result_text'>따뜻한 케미</p>
+          <span className='chemi_character_pic'>
+            {/* 캐릭터 이미지 로직 */}
+          </span>
+          <p className='result_text'>{selectedCharacter.select}</p>
+        </div>
+
+        <div className='chemi_character2'>
+          <p className='result_text'>차가운 케미</p>
+          <span className='chemi_character_pic'>
+            {/* 캐릭터 이미지 로직 */}
+          </span>
+          <p className='result_text'>{selectedCharacter.type}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TestResult;
