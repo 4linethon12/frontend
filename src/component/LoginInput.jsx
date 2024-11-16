1e7d3409c54f93439db88d0d23f6c82a9cb4ecb6
yuchae Park <20232293@sungshin.ac.kr>
2024-11-16 12:57:23 +0900

    feat: 로그인 마스킹

--- src/component/LoginInput.jsx
+++ src/component/LoginInput.jsx
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import ErrorIcon from '/images/Group/Check.svg'; // 조건에 맞지 않을 때의 아이콘
-import SuccessIcon from '/images/Group/Error.svg'; // 조건을 만족할 때의 아이콘
 
 // 부모 컨테이너 스타일 설정
 const InputContainer = styled.div`
@@ -27,27 +25,10 @@ const StyledInput = styled.input`
   }
 `;
 
-// 아이콘 스타일 설정
-const Icon = styled.img`
-  position: absolute;
-  right: 15px; /* 오른쪽에 위치 */
-  top: 50%;
-  transform: translateY(-50%); /* 수직 가운데 정렬 */
-  width: 20px; /* 아이콘 크기 */
-  height: 20px;
-  pointer-events: none; /* 클릭 이벤트 방지 */
-`;
+
 
 const Input = ({ type = 'text', placeholder, value, onChange }) => {
-  // 조건에 따른 아이콘 설정
-  let icon;
-  if (value.length >= 10) {
-    icon = SuccessIcon; // 조건을 만족하면 성공 아이콘
-  } else if (value.length > 0) {
-    icon = ErrorIcon; // 조건에 맞지 않으면 에러 아이콘
-  } else {
-    icon = ''; // 아무것도 입력하지 않으면 아이콘 없음
-  }
+  
 
   return (
     <InputContainer>
